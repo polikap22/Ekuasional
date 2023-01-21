c3ed8bf9f44fa530856343b0085f85270fdf32ee
polikap22 <69667427+polikap22@users.noreply.github.com>
2023-01-20 20:06:40 -0800

    Deleted
    UHhh Keknya harus ada yang diperbaiki dan BANYAK

--- Ekuasional/src/client.h
+++
@@ -1,8 +0,0 @@
-#ifndef _CLIENT_H_
-#define _CLIENT_H_
-
-#include "../include/solve.h"
-int client_square_eq();
-
-// JANGAN DIPEGANG!
-void display_roots(const roots_state rs, const square_solutions solutions);
--- Ekuasional/src/config.h
+++
@@ -1,8 +0,0 @@
-#ifndef CONFIG_H_
-#define CONFIG_H_
-
-#include <cstddef>
-const size_t INPUT_BUFFER_SIZE = 28;
-
-
-#endif
